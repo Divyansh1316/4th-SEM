2c813b9609b72e19593adb5603b780230d57c00c
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2024-02-08 11:41:34 +0530

    Update jump_search.c

--- DAA/jump_search.c
+++ DAA/jump_search.c
@@ -2,33 +2,37 @@
 void main()
 {
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int ele;
+    int ele, flag = 0;
     printf("Enter element to be searched : ");
     scanf("%d", &ele);
-    for (int i = 0; i < 10; i += 2)
+    for (int i = 0; i < 10; i += 3)
     {
         if (arr[i] == ele)
         {
-            printf("Element found");
-            break;
+            flag = 1;
         }
         else if (arr[i] > ele)
         {
 
-            for (int j = i - 2 + 1; j < i; j++)
+            for (int j = i - 3 + 1; j < i; j++)
             {
                 if (arr[j] == ele)
                 {
-                    printf("Element found by linear search");
-                    break;
-                }
-                else
-                {
-                    printf("Element not found");
-                    break;
+                    flag = 2;
                 }
             }
-            break;
         }
+    }
+    if (flag == 1)
+    {
+        printf("element found by jump search");
+    }
+    else if (flag == 2)
+    {
+        printf("element found by linear search");
+    }
+    else
+    {
+        printf("element not found ");
     }
 }
