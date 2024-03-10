96fdba0ecc229ef6c5b307f0028a52f346e0266f
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2024-03-10 14:05:01 +0530

    Update heapsort.c

--- DAA/heapsort.c
+++ DAA/heapsort.c
@@ -1,63 +1,74 @@
 #include <stdio.h>
-void swap(int arr[], int x, int y)
+int comp = 0, swaps = 0;
+void swap(int arr[], int j, int k)
 {
-    int temp = arr[x];
-    arr[x] = arr[y];
-    arr[y] = temp;
+    int temp = arr[j];
+    arr[j] = arr[k];
+    arr[k] = temp;
+    swaps++;
 }
-void heapify(int arr[], int i, int n)
+void maxHeapify(int arr[], int n, int i)
 {
+    int largest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
-    int largest;
-    if (l <= n && arr[l] > arr[i])
+    comp++;
+    if (l < n && arr[l] > arr[largest])
+    {
         largest = l;
-    else
-        largest = i;
-    if (r <= n && arr[r] > largest)
+    }
+    comp++;
+    if (r < n && arr[r] > arr[largest])
+    {
         largest = r;
+    }
     if (largest != i)
     {
-        swap(arr, largest, i);
-        heapify(arr, largest, n);
+        swap(arr, i, largest);
+        maxHeapify(arr, n, largest);
     }
 }
-void buildmaxheap(int arr[], int n)
+void buildMaxHeap(int arr[], int n)
 {
-    for (int i = (n / 2) - 1; i > 0; i--)
+    for (int i = (n / 2) - 1; i >= 0; i--)
     {
-        heapify(arr, i, n);
+        maxHeapify(arr, n, i);
     }
 }
-void heapsort(int arr[], int n)
+void heapSort(int arr[], int n)
 {
-    buildmaxheap(arr, n);
+    buildMaxHeap(arr, n);
     for (int i = n - 1; i > 0; i--)
     {
         swap(arr, 0, i);
-        heapify(arr, 0, i);
+        maxHeapify(arr, i, 0);
     }
 }
-int main()
+void main()
 {
-    int size, i;
-    printf("Enter the size\n");
-    scanf("%d", &size);
-    int arr[size];
-    for (i = 0; i < size; i++)
+    int n, t, k;
+    printf("Enter number of test cases : ");
+    scanf("%d", &t);
+    for (int k = 0; k < t; k++)
     {
-        scanf("%d", &arr[i]);
+        printf("Enter size : ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter elements : ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        heapSort(arr, n);
+        printf("\nSorted array is : \n");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        printf("\nComparisions : %d\n", comp);
+        printf("Swaps : %d\n", swaps);
+        swaps = 0;
+        comp = 0;
     }
-    for (i = 0; i < size; i++)
-    {
-        printf("%d\t", arr[i]);
-    }
-    printf("\n");
-    heapsort(arr, size);
-    for (i = 0; i < size; i++)
-    {
-        printf("%d\t", arr[i]);
-    }
-
-    return 0;
 }
