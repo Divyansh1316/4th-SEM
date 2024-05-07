b57727cc72fd87392a94b8b8bd07addc9f6a299d
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2024-05-07 09:34:33 +0530

    Update task10_3.java

--- JAVA/task10_3.java
+++ JAVA/task10_3.java
@@ -11,6 +11,21 @@ Output  will be - 4
 import java.io.*;
 
 public class task10_3 {
+    static boolean isPalindrome(String str) {
+        int low = 0;
+        int high = str.length() - 1;
+
+        // Keep comparing characters while they are same
+        while (low < high) {
+            if (str.charAt(low) != str.charAt(high)) {
+                return false; // not a palindrome.
+            }
+            low++; // move the low index forward
+            high--; // move the high index backwards
+        }
+        return true; // is a palindrome
+    }
+
     public static void main(String[] args) {
         try {
             FileReader fr = new FileReader("/home/geu/Desktop/d312/myfile.txt");
@@ -26,7 +41,7 @@ public class task10_3 {
             System.out.println("\n\n" + words);
             int start = 0, vword = 0;
             for (int i = 0; i < words.length(); i++) {
-                if (words.charAt(i) == ' ') {
+                if (words.charAt(i) == ' ' || words.charAt(i) == '\n') {
                     word = words.substring(start, i);
                     if (word.length() > 0) {
                         start = i + 1;
