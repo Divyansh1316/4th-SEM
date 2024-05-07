7337a3d571c0a1b25b58e9bdd09a6d4b484d8136
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2024-05-07 09:05:58 +0530

    Add files via upload

---
+++ JAVA/task10_3.java
@@ -0,0 +1,47 @@
+/* 
+3. Implement a java file handling program to count and display the number of palindromes present in a text file "myfile.txt". 
+Example: If the file "myfile.txt" contains the following lines,
+My name is NITIN
+Hello aaa and bbb word
+How are You
+ARORA is my friend
+Output  will be - 4
+*/
+
+import java.io.*;
+
+public class task10_3 {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("/home/geu/Desktop/d312/myfile.txt");
+            String words = "";
+            String word;
+            int k = fr.read();
+            words += (char) k;
+            while (k != -1) {
+                System.out.print((char) k);
+                k = fr.read();
+                words += (char) k;
+            }
+            System.out.println("\n\n" + words);
+            int start = 0, vword = 0;
+            for (int i = 0; i < words.length(); i++) {
+                if (words.charAt(i) == ' ') {
+                    word = words.substring(start, i);
+                    if (word.length() > 0) {
+                        start = i + 1;
+                    }
+                    for (int j = 0; j < word.length() / 2; j++) {
+                        if (word.charAt(j) == word.charAt(word.length() - j - 1)) {
+                            vword++;
+                        }
+                    }
+                }
+            }
+            System.out.println("Output : " + vword);
+            fr.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
